7275c2bb922321d063e2c4c5ef8181cf58bf8cb2
evakt <evaktar4@gmail.com>
2025-08-06 16:46:32 +0100

    Updated instructions

--- FictionArt - Experiment 2/experiment2/demographics.js
+++ FictionArt - Experiment 2/experiment2/demographics.js
@@ -38,7 +38,7 @@ const demographics_consent_exp2 = {
             "Thank you for considering to take part in this study conducted by Dr Dominique Makowski from the University of Sussex and his team (see contact information below).</p>" +
             // Description
             "<p align='left'><b>Why have I been invited and what will I do?</b><br>" +
-            "The goal is to study how <b>new technology impacts human appreciation of art</b>.  In this study, you will complete a visual task where [INSERT]. Then you will complete some questionnaires, including [INSERT]." +
+            "The goal is to study how <b>new technology impacts human appreciation of art</b>.  In this study, you will complete a visual task where you will view and identify artworks." +
             "The whole experiment will take you <b style='color:#FF5722;'>~45 min</b> to complete. Please make sure that you are <b>attentive and in a quiet environment</b>, and that you have time to complete it in one go.</p>" +
             // Results and personal information
             "<p align='left'><b>What will happen to the results and my personal information?</b><br>" +
